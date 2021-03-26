3f1134302d5175151d2a92f0bb1cd30d74eb789f
vaidik kamde <vaidik16206138@gmail.com>
2021-03-27 00:47:25 +0530

    Added required to form.control in Inputform.jsx

--- components/Inputform.jsx
+++ components/Inputform.jsx
@@ -5,7 +5,7 @@ export default function Inputform(props){
         <Form.Row>
           <Form.Group as={Col} controlId="rollnumber">
             <Form.Label>Roll Numbers</Form.Label>
-            <Form.Control type="text" placeholder="For Example 5,6,9" onChange={e => props.setRollNumbers(e.target.value)}/>
+            <Form.Control required type="text" placeholder="For Example 5,6,9" onChange={e => props.setRollNumbers(e.target.value)}/>
             <Form.Text className="text-muted">
               Enter Roll Numbers as "," sepreated values.
             </Form.Text>
